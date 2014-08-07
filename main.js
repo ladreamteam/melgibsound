58fc1c9d3fd89d1c83d4a21ecf21b2d09cf02b2a
AlixBa <alix.baggio@outlook.fr>
2014-08-07 23:41:45 +0200

    use onLoad to wait for the page to load

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-window.MELGIBSOUND_DEBUG = false;
+(function () {
+    window.addEventListener(
+        'load',
+        function () {
+            window.MELGIBSOUND_DEBUG = false;
 
-// init script
-var Dock = require('./scripts/Dock.js');
-window.MELGIBSOUND_DOCK = window.MELGIBSOUND_DOCK || new Dock;
-window.MELGIBSOUND_DOCK.init();
+            // init script
+            var Dock = require('./scripts/Dock.js');
+            window.MELGIBSOUND_DOCK = window.MELGIBSOUND_DOCK || new Dock;
+            window.MELGIBSOUND_DOCK.init();
+        }
+    );
+})();
