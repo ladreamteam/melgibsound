83ed53b9ad346508276c2d86d678ba449b61c46f
AlixBa <alix.baggio@outlook.fr>
2014-08-08 13:04:51 +0200

    specific launch if chrome.

--- main.js
+++ main.js
@@ -5,18 +5,25 @@
         return;
     }
 
-    // else start!
-    var $ = require('jquery');
-    $(document).ready(
-        function () {
-            window.MELGIBSOUND_DEBUG = false;
+    // start function
+    var melgibsound = function () {
+        window.MELGIBSOUND_DEBUG = false;
 
-            // init script
-            if (!window.MELGIBSOUND_DOCK) {
-                var Dock = require('./scripts/Dock.js');
-                window.MELGIBSOUND_DOCK = new Dock;
-                window.MELGIBSOUND_DOCK.init();
-            }
+        // init script
+        if (!window.MELGIBSOUND_DOCK) {
+            var Dock = require('./scripts/Dock.js');
+            window.MELGIBSOUND_DOCK = new Dock;
+            window.MELGIBSOUND_DOCK.init();
         }
-    );
+    };
+
+    // if chrome use jQuery
+    if ((/chrome/i).test(navigator.userAgent)) {
+        var $ = require('jquery');
+        $(document).ready(melgibsound);
+    }
+    else {
+        window.addEventListener('load', melgibsound, false);
+    }
+
 })();
